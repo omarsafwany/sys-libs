91353bfc5bc65b7cadd14d6b0b40974d204c02b3
Omar Safwany <omar.safwany@gmail.com>
2017-10-01 17:31:26 +0200

    reading Gemfile to fetch gems

--- lib/sys_libs.rb
+++ lib/sys_libs.rb
@@ -1,5 +1,19 @@
-require "sys_libs/version"
+#require "sys_libs/version"
+require "unirest"
 
 module SysLibs
-  # Your code goes here...
+    @packagesArray = []
+    f = File.open("Gemfile", "r")
+    f.each_line do |line|
+      if !line.include?"#" and line.include? "gem" and !line.include?"rubygems.org" and !line.include? "gemspec"
+        name = line.split(" ")[1].slice(1..-2)
+        if name[-1].chr == "'"
+          name = name.slice(0..-2)
+        end
+        @packagesArray << (name)
+      end
+    end
+    f.close
+    
+    
 end
