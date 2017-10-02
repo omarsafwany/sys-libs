10bd245eaa66a725397e01228486514fbe46e775
Omar Safwany <omar.safwany@gmail.com>
2017-10-03 00:00:08 +0200

    adding class

--- lib/sys_libs.rb
+++ lib/sys_libs.rb
@@ -2,51 +2,53 @@ require "sys_libs/version"
 require "rest-client"
 
 module SysLibs
-    #Read the project's Gemfile, fetch all uncommented gems and add to array
-    def getPackages
-        @packagesArray = []
-        f = File.open("Gemfile", "r")
-        f.each_line do |line|
-          if !line.include?"#" and line.include? "gem" and !line.include?"rubygems.org" and !line.include? "gemspec"
-            name = line.split(" ")[1].slice(1..-2)
-            if name[-1].chr == "'"
-              name = name.slice(0..-2)
+    class Task
+        #Read the project's Gemfile, fetch all uncommented gems and add to array
+        def getPackages
+            @packagesArray = []
+            f = File.open("Gemfile", "r")
+            f.each_line do |line|
+              if !line.include?"#" and line.include? "gem" and !line.include?"rubygems.org" and !line.include? "gemspec"
+                name = line.split(" ")[1].slice(1..-2)
+                if name[-1].chr == "'"
+                  name = name.slice(0..-2)
+                end
+                @packagesArray << (name)
+              end
             end
-            @packagesArray << (name)
-          end
+            f.close
+            return @packagesArray
         end
-        f.close
-        return @packagesArray
-    end
-    
-    #Get the current os
-    def getOS
-        @os ||= (
-          host_os = RbConfig::CONFIG['host_os']
-          case host_os
-          when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-            :windows
-          when /darwin|mac os/
-            :mac
-          when /linux/
-            :linux
-          when /solaris|bsd/
-            :unix
-          else
-            raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
-          end
-        )
-    end
-    
-    #Send a post request to the server to retrieve the required sys libs
-    def getMissingLibs(packagesArray, os)
-        response = RestClient.post "https://stormy-bayou-25992.herokuapp.com/packages/search", 
-                            { :packages => packagesArray, :os => os }
-        body = JSON.parse(response.body)
-        body.each do |key, value|
-            puts key["name"] + " needs the following sys libs to be installed:"
-            key["dependencies"].each do |d|
-                puts d["name"]
+
+        #Get the current os
+        def getOS
+            @os ||= (
+              host_os = RbConfig::CONFIG['host_os']
+              case host_os
+              when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+                :windows
+              when /darwin|mac os/
+                :mac
+              when /linux/
+                :linux
+              when /solaris|bsd/
+                :unix
+              else
+                raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
+              end
+            )
+        end
+
+        #Send a post request to the server to retrieve the required sys libs
+        def getMissingLibs(packagesArray, os)
+            response = RestClient.post "https://stormy-bayou-25992.herokuapp.com/packages/search", 
+                                { :packages => packagesArray, :os => os }
+            body = JSON.parse(response.body)
+            body.each do |key, value|
+                puts key["name"] + " needs the following sys libs to be installed:"
+                key["dependencies"].each do |d|
+                    puts d["name"]
+                end
             end
         end
     end
